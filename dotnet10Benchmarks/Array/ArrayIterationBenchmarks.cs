7d496fbd61d09af4b9e9aee6a80c643ca05d38b6
David Callan <davepcallan@gmail.com>
2025-02-20 16:57:04 +0000

    Add int array iteration benchmark showing .NET 10 ~ x2 as fast as .NET 9

---
+++ dotnet10Benchmarks/Array/ArrayIterationBenchmarks.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+
+[Config(typeof(Config))]
+[MemoryDiagnoser]
+[HideColumns(Column.Job, Column.RatioSD, Column.Gen0, Column.AllocRatio)]
+[SimpleJob(RuntimeMoniker.Net90,baseline:true)]
+[SimpleJob(RuntimeMoniker.Net10_0)]
+public class ArrayIterationBenchmarks
+{
+    static readonly int[] s_ro_array = new int[1000];
+
+    [Benchmark]
+    public int ForeachStaticReadonlyArray()
+    {
+        int sum = 0;
+        foreach (int i in s_ro_array) sum += i;
+        return sum;
+    }
+
+    private class Config : ManualConfig
+    {
+        public Config()
+        {
+            SummaryStyle = SummaryStyle.Default.WithRatioStyle(RatioStyle.Percentage);
+        }
+    }
+}
